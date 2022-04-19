21f4a83dfe77ec6209379de812ad910338700485
Nikita <skrytyya@gmail.com>
2022-04-19 19:14:10 +0500

    syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,25 @@
 export function romanToInteger(str) {
-    let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
-    return result;
+    let roman = {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    };
+    return str
+        .toUpperCase()
+        .split('')
+        .reduce(function (s, v, i, arr) {
+            const [a, b, c] = [
+                roman[arr[i]],
+                roman[arr[i + 1]],
+                roman[arr[i + 2]],
+            ];
+            return b > a ? s - a : s + a;
+        }, 0);
 }
